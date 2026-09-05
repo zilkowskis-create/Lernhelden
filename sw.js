a49a358b5c5e027166aa885cef0296a231656517
zilkowskis-create <zilkowskis@gmail.com>
2026-09-05 23:35:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lernhelden-v8-8-bravo';
+const CACHE='lernhelden-v8-9-mathe-fix';
 const CORE=['./','./index.html','./manifest.webmanifest','./config.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
